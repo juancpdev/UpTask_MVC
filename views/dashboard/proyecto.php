04c1e4c4fe9d3748bfe1704606ff301a09379486
Juan Cruz <juan_pineda_12@hotmail.es>
2023-10-08 23:28:21 -0300

    modal-tareas

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -1,7 +1,17 @@
 <?php include_once __DIR__ . "/header-dash.php" ?>
     <h3> <?php echo $proyecto->proyecto ?> </h3>
 
-    <div class="contenedor-volver">
-        <a href="/dashboard">Volver</a>
+    <div class="contenedor-sm contenedor-proyecto">
+        <div class="contenedor-tarea">
+            <button id="boton-tarea" class="boton-tarea" type="button">+ Nueva Tarea</button>
+        </div>
+
+        
+        
     </div>
+
 <?php include_once __DIR__ . "/footer-dash.php" ?>
+
+<?php 
+    $script = '<script src="build/js/tarea.js"></script>';    
+?>
