2a6fef8a9f3204abce75f5130ffa0a4e1c21b8fe
Juan Cruz <juan_pineda_12@hotmail.es>
2024-02-28 18:28:39 -0300

    favicon

--- views/layout.php
+++ views/layout.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Open+Sans&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="build/css/app.css">
+    <link rel="icon" href="/build/img/favicon.png" type="image/png">
 </head>
 <body>
 
